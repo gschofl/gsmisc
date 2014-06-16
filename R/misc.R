05871f75effb1563ac033908456e2bb7434d8501
Gerhard Schöfl <gerhard.schofl@gmail.com>
2014-06-16 11:23:25 +0200

    Add google search from the R command line

--- R/misc.R
+++ R/misc.R
@@ -24,3 +24,27 @@ plotInheritance <- function (classes, subclasses = FALSE, ...) {
   invisible(graph)
 }
 
+#' Use Google Search from the R command line.
+#' 
+#' @param query Search query
+#' @param n Max number of results returned.
+#' @return Prints the URLs returned by Google Search to screen and invisibly
+#' returns a character vector of the URLs. 
+#' @examples
+#' r <- google("use google from command line")
+#' \dontrun{
+#' browseURL(r[1])
+#' }
+google <- function(query = "", n = 10) {
+  stopifnot(require(RCurl))
+  h <- basicTextGatherer()
+  url <- paste0('https://www.google.de/search?tbs=li:1&q=', curlEscape(query))
+  curlPerform(url = url, writefunction = h$update, .opts = list(timeout = 6))
+  ## Print the matching parts on a separate output line each
+  stream <- gsub("&amp", "", gsub("/url?q=", "", system("grep -oP '\\/url\\?q=.+?&amp'", input = h$value(), intern = TRUE), fixed = TRUE))
+  if (length(stream) > n)
+    stream <- stream[1:n]
+  print(stream)
+  return(invisible(stream))
+}
+
