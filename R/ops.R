ecc8fcb63a84ab5e72fc12f64a4e9c902d1d90b8
Gerhard Schöfl <gerhard.schofl@gmail.com>
2014-09-12 09:37:19 +0200

    Remove `:=` and re-roxygenise

--- R/ops.R
+++ R/ops.R
@@ -14,44 +14,16 @@ NULL
   ifelse(filter(a), b, a)
 }
 
-
 #' @export
 "%|null|%" <- Partial(`%||%`, filter="is.null")
 
-
 #' @export
 "%|na|%" <- Partial(`%||%`, filter="is.na")
-
 
 ## Chain functions
 #' @export
 "%@%" <- function(x, f) {
   eval.parent(as.call(append(as.list(substitute(f)), list(x), 1)))
-}
-
-
-# Pinched from http://code.google.com/p/miscell/source/browse/rvalues/rvalues.r
-#' @export
-":=" <- function(lhs, rhs) {
-  frame <- parent.frame()
-  lhs <- as.list(substitute(lhs))
-  if (length(lhs) > 1) {
-    lhs <- lhs[-1]
-  }
-  if (length(lhs) == 1) {
-    assign(as.character(lhs[[1]]), rhs, envir=frame)
-    return(invisible(NULL))
-  }
-  if (is.function(rhs) || is(rhs, 'formula')) {
-    rhs <- list(rhs)
-  }
-  if (length(lhs) > length(rhs)) {
-    rhs <- c(rhs, rep(list(NULL), length(lhs) - length(rhs)))
-  }
-  for (i in seq_along(lhs)) {
-    assign(as.character(lhs[[i]]), rhs[[i]], envir=frame)
-  }
-  return(invisible(NULL))
 }
 
 ## http://stackoverflow.com/questions/7519790/assign-multiple-new-variables-in-a-single-line-in-r
