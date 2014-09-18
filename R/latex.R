45785da48eff11b3333bec97392a67fdea3aa1a0
Gerhard Schöfl <gerhard.schofl@gmail.com>
2014-09-18 15:33:46 +0200

    Add latex

---
+++ R/latex.R
@@ -0,0 +1,21 @@
+#' @importFrom xtable xtable print.xtable
+NULL
+
+#' Generate and print a LaTeX or HTML table.
+#'
+#' A function that wraps \code{\link[xtable]{xtable}} and \code{\link[xtable]{print.xtable}},
+#' to make it easy to use in "Rmarkdown" dokuments.
+#'   
+#' @details The output style (LaTeX or HTML) is controlled via the option
+#' \code{options(xtable.type = "latex")} or \code{options(xtable.type = "html")}
+#' 
+#' @inheritParams xtable::xtable
+#' @return Prints the table to stdout.
+#' @export
+latex <- function(df, caption = NULL, label = NULL, align = NULL, digits = NULL,
+                  display = NULL, ...) {
+  tbl <- xtable(df, caption = caption, label = label, align = align,
+                digits = digits, display = display)
+  print.xtable(tbl, ..., comment = FALSE)
+}
+
